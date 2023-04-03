86086722a199cd1e2b19e548acbc4c8e63732a68
“LimisiMike” <michaellimisi7@gmail.com>
2023-04-03 10:12:25 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+
+function isPalindrome(word){
+  //iterate from the beginning to the  middle
+  for (let i = 0; i < word.length/2; i++){
+    //let's check each letter from start and compare iw with one from the end
+    const j = word.length -1 -i;
+    //if letters don't match, we return false
+    if (word[i] !==word[j]) return false;
+  }
+  return true;
+  }
 
 /* 
   Add your pseudocode here
+  - We're going to iterate over the string and our iteration checks to see if the first letter matches the last one
+  if letters dont match, return false,
+  else,return true
 */
 
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
